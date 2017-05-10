9648bc51ed9da4eed10fc5b1e5209b4bc645135a
Viacheslav Popika <popikeyshen@gmail.com>
2017-05-10 03:38:24 +0300

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+char message[1024];
+char buf[sizeof(message)];
+
+using namespace std;
+int main()
+{
+    int sock;
+    struct sockaddr_in addr;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0)
+    {
+        perror("socket");
+        exit(1);
+    }
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(3427); // или любой другой порт...
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("connect");
+        exit(2);
+    }
+
+    while(message[0]!='q')
+    {
+        cin>>message;
+        send(sock, message, sizeof(message), 0);
+        //recv(sock, buf, sizeof(message), 0);
+    }
+
+    printf(buf);
+    close(sock);
+
+    return 0;
+}
